f98cd71605ea329b687c0dd17d4dcd8387e60d29
dalyDai <2508291029@qq.com>
2021-05-10 22:07:36 +0800

    feat: 个人中心页面搭建

--- src/views/personal/index.vue
+++ src/views/personal/index.vue
@@ -1,37 +1,105 @@
 <template>
-  <div>
-    <p class="aaa">个人中心</p>
-    <van-button size="small" @click="onOpenUserPage">路由跳转</van-button>
-    <div>{{ NAME_OF_CONSTANT[0].name }}&nbsp;{{ OTHER }}</div>
+  <div :class="$style.person">
+    <div :class="$style.personTop">
+      <base-svg-icon
+        iconName="iconshezhi"
+        :cls="$style.personConfig"
+      ></base-svg-icon>
+    </div>
+    <div :class="$style.personHead">
+      <div>
+        <img :src="userImage" alt="" />
+      </div>
+      <p>{{ userName }}</p>
+    </div>
+    <p :class="$style.personName">我的交易</p>
+    <div :class="$style.personGoods">
+      <div>
+        <base-svg-icon iconName="iconfabu1"></base-svg-icon>
+        <p>已发布</p>
+      </div>
+      <div>
+        <base-svg-icon iconName="iconfukuan"></base-svg-icon>
+        <p>我卖出的</p>
+      </div>
+      <div>
+        <base-svg-icon iconName="iconxinyongka"></base-svg-icon>
+        <p>我买到的</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  // 常量对象
-  const: {
-    NAME_OF_CONSTANT: [{ name: '123' }, { age: 16 }],
-    OTHER: 'value2'
+  data() {
+    return {
+      userName: '',
+      userImage: ''
+    };
+  },
+  created() {
+    this.initPage();
   },
   methods: {
-    onOpenUserPage() {
-      console.info(this.$route);
-      // this.$router.addRoutes([
-      //   {
-      //     path: '/ioio',
-      //     name: 'ioio',
-      //     component: () => import('../needs/index.vue')
-      //   }
-      // ]);
-      // this.$router.push('ioio');
+    initPage() {
+      const data = this.$store.getters['platform/getData'];
+
+      this.userName = data.name;
+      this.userImage = data.avatar;
     }
   }
 };
 </script>
 
-<style>
-.aaa {
-  color: red;
-  font-size: 14px;
+<style lang="less" module>
+.person {
+  .full-y;
+  &-top {
+    background: url('/static/images/homeBg.png') no-repeat;
+    background-size: 100%;
+    .flex;
+    justify-content: flex-end;
+    padding: 20px 70px;
+
+    .person-config {
+      font-size: 20px;
+    }
+  }
+
+  &-head {
+    background: url('/static/images/homeBg.png') no-repeat;
+    background-size: 100%;
+    height: 20%;
+    padding: 50px 0;
+    text-align: center;
+    > div {
+      width: 380px;
+      height: 380px;
+      border-radius: 55%;
+      margin: 0 auto;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 55%;
+      }
+    }
+  }
+
+  &-name {
+    // padding-left: 40px;
+    margin: 0;
+    padding: 60px;
+    background: #fff;
+  }
+
+  &-goods {
+    background: #fff;
+    .flex;
+    > div {
+      text-align: center;
+      flex: 1;
+    }
+  }
 }
 </style>
