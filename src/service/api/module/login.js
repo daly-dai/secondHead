2cc37616a1fe7ec98438861b7c410cb4a838339d
dalyDai <2508291029@qq.com>
2021-05-15 19:02:01 +0800

    feat: 登录接口对接

--- src/service/api/module/login.js
+++ src/service/api/module/login.js
@@ -2,24 +2,24 @@
  * 系统登录 api 文档接口 领域模型
  */
 export default [
-  // {
-  //   name: 'doLogin',
-  //   method: 'POST',
-  //   desc: '用户登录',
-  //   path: '/users/login',
-  //   data: { email: '', password: '' },
-  //   mock: false,
-  //   isLogin: true, // 登录接口
-  //   isWhite: true // 白名单 （设置为 true 则会去除 token 参数）
-  // },
-  // {
-  //   name: 'logout',
-  //   method: 'POST',
-  //   desc: '用户登出',
-  //   path: '/user/logout',
-  //   mockPath: '/mock/user/logout',
-  //   headers: { token: '' }
-  // },
+  {
+    name: 'doLogin',
+    method: 'POST',
+    desc: '用户登录',
+    path: '/users/login',
+    data: { email: '', password: '' },
+    mock: false,
+    isLogin: true, // 登录接口
+    isWhite: true // 白名单 （设置为 true 则会去除 token 参数）
+  },
+  {
+    name: 'logout',
+    method: 'POST',
+    desc: '用户登出',
+    path: '/user/logout',
+    mockPath: '/mock/user/logout',
+    headers: { token: '' }
+  },
   {
     name: 'register',
     path: '/users/register',
